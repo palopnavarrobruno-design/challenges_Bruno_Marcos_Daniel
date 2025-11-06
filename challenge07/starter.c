8dd18e744b65c8aac3199a6cec013fc38fc2b51d
Marcos Scurtu <132715054+Shurkon@users.noreply.github.com>
2025-11-06 12:46:34 +0100

    Add files via upload

---
+++ challenge07/starter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+extern int suma(int a, int b) {
+
+    int resultado;
+
+    resultado = a + b;
+
+    return resultado;
+
+}
+
+# if EXISTS == 1
+  void pruebas();
+#endif
+
+
+int main() {
+
+  #if EXISTS == 1
+  pruebas();
+  #endif
+
+
+  return 0;
+
+}
